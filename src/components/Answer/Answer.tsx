49aca7aaaae06b67f26d8b7a70b1d0d6663006c2
pratyushplp <pratyushplp@gmail.com>
2023-06-29 15:01:27 -0500

    Added ratings and feedback for answers

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,9 +1,11 @@
-import { useMemo } from "react";
-import { Stack, IconButton } from "@fluentui/react";
+import { useMemo,useState } from "react";
+import { Stack, StackItem} from "@fluentui/react";
 import DOMPurify from "dompurify";
 import styles from "./Answer.module.css";
 import { AskResponse, getCitationFilePath } from "../../api";
 import { parseAnswerToHtml } from "./AnswerParser";
+import {LikeOutlined,DislikeOutlined} from "@ant-design/icons"
+import {Button,Input} from "antd"
 
 interface Props {
     answer: AskResponse;
@@ -16,13 +18,39 @@ export const Answer = ({
     isSelected,
     onCitationClicked,
 }: Props) => {
+
+    const { TextArea } = Input;
+
+
+    const [clickedList, setClickedList] = useState<boolean[]>([false,false])
+    const [showFeedback, setShowFeedback] = useState<boolean>(false)
     const parsedAnswer = useMemo(() => parseAnswerToHtml(answer.answer, onCitationClicked), [answer]);
 
     const sanitizedAnswerHtml = DOMPurify.sanitize(parsedAnswer.answerHtml);
 
+    const updateClickedList = (e:any) =>
+    {
+        console.log(e)
+        // console.log(((e.target as HTMLButtonElement)))
+        const index = e.currentTarget.value as unknown as number
+        setClickedList((prev)=> prev.map((item,idx)=> idx==index?!item:item))
+        console.log(index)
+        console.log(clickedList)
+    }
+const feedbackClicked = (e:any) =>
+{
+    setShowFeedback((prev)=> !prev)
+}
+
     return (
         <Stack className={`${styles.answerContainer} ${isSelected && styles.selected}`} verticalAlign="space-between">
             {!!parsedAnswer.citations.length && (
+                <>
+                <Stack.Item >
+                    <p>
+                        Hello World!
+                    </p>
+                </Stack.Item>
                 <Stack.Item>
                     <Stack horizontal wrap tokens={{ childrenGap: 5 }}>
                         <span className={styles.citationLearnMore}>Citations:</span>
@@ -36,7 +64,25 @@ export const Answer = ({
                         })}
                     </Stack>
                 </Stack.Item>
+                <Stack.Item className={styles.extras}>
+        <Button value={"0"} shape="circle" icon={<LikeOutlined/>}   onClick={updateClickedList} style={{backgroundColor: clickedList[0] ?"lightblue":"" }} />
+        <div className={styles.rateButton}>
+        <Button value={"1"} shape="circle" icon={<DislikeOutlined/>}  onClick={updateClickedList} style={{backgroundColor: clickedList[1]?"lightblue":"" }} />
+        </div>
+        </Stack.Item>
+        <Stack.Item className={styles.feedback}>
+            <Button onClick={feedbackClicked} className={styles.feedbackButton} style={{backgroundColor: showFeedback ?"lightblue":"" }}>
+                Feedback
+            </Button>
+            {showFeedback &&
+            <div >
+            <TextArea rows={4} />
+            <Button className={styles.feedbackSend} type="primary" size="small">send</Button>
+            </div>}
+        </Stack.Item>
+                </>
             )}
+
         </Stack>
     );
 };
